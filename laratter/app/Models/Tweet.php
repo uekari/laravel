f6030b9226d76a878e038920be5fb0d7d6805b9c
上仮屋遥 <hrkr0472@gmail.com>
2022-12-27 12:31:49 +0900

    1227

--- laratter/app/Models/Tweet.php
+++ laratter/app/Models/Tweet.php
@@ -23,6 +23,17 @@ class Tweet extends Model
     {
     return self::orderBy('updated_at', 'desc')->get();
     }
+
+    public function user()
+    {
+    return $this->belongsTo(User::class);
+    }  
+
+    public function users()
+    {
+    return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
 }
 
 
